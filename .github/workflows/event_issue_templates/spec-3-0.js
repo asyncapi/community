785684d372739ccb95493ca98f1465f1a3c77414
Lukasz Gornicki <lpgornicki@gmail.com>
2022-01-14 18:14:26 +0100

    chore: add automation for new 3.0 release related meeting (#224)

---
+++ .github/workflows/event_issue_templates/spec-3-0.js
@@ -0,0 +1,30 @@
+module.exports.getMeetingIssueContent = (time, everytimezone) => {
+
+    return `This is the meeting for community member involved in works related to 3.0 release of AsyncAPI Specification. 
+    
+**The meeting takes place bi-weekly on Wednesdays**. First and third week every month until release. Recordings from the previous meetings are available in [this](https://www.youtube.com/playlist?list=PLbi1gRlP7pihClJY-kXuTRRJ8n1awb0VV) playlist on YouTube.
+
+**This time we meet at [${time} UTC](${everytimezone})**
+
+Join [this mailing list](https://groups.google.com/forum/#!forum/asyncapi-users) to get an always-up-to-date invite to the meeting in your calendar. You can also check [AsyncAPI Calendar](https://calendar.google.com/calendar/u/0/embed?src=tbrbfq4de5bcngt8okvev4lstk@group.calendar.google.com).
+
+## Agenda
+
+> Don't wait for the meeting to discuss topics that already have issues. Feel free to comment on them earlier.
+
+1. Q&A
+1. _Place for your topic_
+1. Q&A
+
+## Notes
+
+tbd
+
+## Chat
+
+tbd
+
+## Recording
+
+tbd`
+}
