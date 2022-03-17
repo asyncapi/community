f3afca2df752ee8cd2c2c6f3eca74a6a248f6e60
derberg <lpgornicki@gmail.com>
2022-03-17 11:40:48 +0100

    add some logging

--- .github/workflows/event_issue_templates/index.js
+++ .github/workflows/event_issue_templates/index.js
@@ -9,7 +9,12 @@ const { parseDate } = require('./utils/date.js');
  * @param {string} getMeetingIssueContent Function that returns content of the meeting issue 
 */
 module.exports = (date, time, core, getMeetingIssueContent) => {
+    
+    core.info(`Workflow triggered with the following hour ${time} and date ${date}`);
     const dateDetails = parseDate(`${ date }T${ time }:00:00Z`);
+    
+    core.info('This is how time and date looks like after parsing:');
+    core.info(JSON.stringify(dateDetails));
 
     if (dateDetails === 'Invalid Date') core.setFailed('Invalid date of the event. Make sure that you provided correct hour of the meeting and date in a format described in the meeting input form.')
 
